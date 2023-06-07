eea565459c30838dff0be9435b402c484466695d
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-06-07 12:46:35 +0100

    Restore '6/7/2023, 10:27:50 AM Initial files for MakeCode project'

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,44 @@ namespace myImages {
 . . . . c b 5 5 5 5 b c . . . . 
 . . . . . f f f f f f . . . . . 
 `;
+            case "image4":
+            case "witch left":return img`
+. . . . . . . c c . . . . . . . 
+. . . . . . c 5 c . . . . . . . 
+. . . . c c 5 5 5 c c c . . . . 
+. . c c c c 5 5 5 5 c b c c . . 
+. c b b 5 b 5 5 5 5 b 5 b b c . 
+. c b 5 5 b b 5 5 b b 5 5 b c . 
+. . c 5 5 5 b b b b 5 5 5 f . . 
+. . . f 5 5 5 5 5 5 5 5 f f . . 
+. . . . f e e e f b e e f f . . 
+. . . . f e b b f 1 b f f f . . 
+. . . . f b b b b b b f f . . . 
+. . . . . f e e e e f e e . . . 
+. . . . . f 5 b b e b b e . . . 
+. . . . f 5 5 5 5 e b b e . . . 
+. . . . c b 5 5 5 5 e e . . . . 
+. . . . . f f f f f f . . . . . 
+`;
+            case "image5":
+            case "witch right":return img`
+. . . . . . . c c . . . . . . . 
+. . . . . . . c 5 c . . . . . . 
+. . . . c c c 5 5 5 c c . . . . 
+. . c c b c 5 5 5 5 c c c c . . 
+. c b b 5 b 5 5 5 5 b 5 b b c . 
+. c b 5 5 b b 5 5 b b 5 5 b c . 
+. . f 5 5 5 b b b b 5 5 5 c . . 
+. . f f 5 5 5 5 5 5 5 5 f . . . 
+. . f f e e b f e e e f . . . . 
+. . f f f b 1 f b b e f . . . . 
+. . . f f b b b b b b f . . . . 
+. . . e e f e e e e f . . . . . 
+. . . e b b e b b 5 f . . . . . 
+. . . e b b e 5 5 5 5 f . . . . 
+. . . . e e 5 5 5 5 b c . . . . 
+. . . . . f f f f f f . . . . . 
+`;
             case "image3":
             case "mushroom":return img`
 . . . . b b b b . . . . . . . . 
